086014e65ac7aed2b7670b3c66b3cff9b14d16f8
João Victor Nogueira Guedes <146042649+leftyvek21@users.noreply.github.com>
2023-10-06 16:20:35 -0300

    Add files via upload
    Atualizei um pouco mais meu site

---
+++ script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal("#conteiner1", { rotate: { x: 100, y: 0, z: 0 }, duration: 2000 });
+
+sr.reveal("#conteiner2", { rotate: { x: 100, y: 0, z: 0 }, duration: 2000 });
+
+sr.reveal("#conteiner3", { rotate: { x: 100, y: 0, z: 0 }, duration: 2000 });
+
+sr.reveal("#título1", { rotate: { x: 100, y: 0, z: 0 }, duration: 2000 });
+
+sr.reveal("#introdução", { rotate: { x: 100, y: 0, z: 0 }, duration: 2000 });
+
+sr.reveal("#título4", { rotate: { x: 100, y: 0, z: 0 }, duration: 2000 });
